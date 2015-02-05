3c002b61ada024ccac058919cd895850a7e3e8a1
Justin Payne <justin.payne@fda.hhs.gov>
2015-02-05 14:40:09 -0500

    new tool for adding new runs

---
+++ add_run.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import xmlrpclib
+import shutil
+import sys
+import os, os.path
+import glob
+from os.path import join as j
+
+server = xmlrpclib.ServerProxy('http://cfe1019692:8080')
+
+session_key = server.open_deferred_accept('', {}, False)
+
+usage = """
+add_run 0.1
+Justin Payne
+Feb 5, 2015
+
+usage:
+	add_run <CFSAN#> [options] [<file1> [<file2>]] [options]
+	
+options:
+	-t <type>		: type of run (pacbio, iontorrent, 454, *miseq*, nextseq, hiseq)
+	-<Field>=<Value>: edit created run record after processing by setting <Field> to <Value>
+	
+"""
+
+flags = ('-t', 'type')
+post_load_updates = {}
+
+run_types = {'pacbio':'PacBio RS sequence',
+			 'miseq':'Illumina MiSeq sequence',
+			 'iontorrent':'IonTorrent PGM sequence',
+			 '454':'Roche 454 sequence',
+			 'nextseq':'Illumina NextSeq sequence',
+			 'hiseq':'Illumina HiSeq sequence',
+			 }
+
+if 'Darwin' in platform.system():
+	genomics_path = "/Volumes/dna/gnome2/"
+elif 'Linux' in platform.system():
+	genomics_path = "/shared/gn2/"
+else: #Windows
+	genomics_path = "Z:/"
+	
+	
+if __name__ == '__main__':
+	try:
+		id = sys.argv[1]
+	except IndexError:
+		print usage
+		quit()
+	for flag, value in flags:
+		if flag in sys.argv:
+			globals()[value] = sys.argv.pop(sys.argv.index(flag) + 1))
+			sys.argv.remove(flag)
+	for field_param in copy(sys.argv[1:]):
+		if '-' in field_param:
+			field, value = field_param.split('-')[1].split('=')
+			post_load_updates[field] = value
+			sys.argv.remove(field_param)
+	try:
+		files = copy(sys.argv[1:])
+		if not files:
+			files = glob.glob(id + '*fastq*')
+		if not files:
+			print "No files found to associate."
+			server.deferred_accept_rollback(session_key)
+			print usage
+			quit()
+		run_type = run_types[type]
+		
+		runid, path = server.deferred_accept(id, {'data_type':run_type,
+												  'job_type':'SPAdes',
+												  'trimmer':'basic_trimmer',
+												  'raw_file':','.join([os.path.basename(f) for f in files]),
+												  'file0':''.join(files[0:1]),
+												  'file1':''.join(files[1:2]),
+												  'assemble':True}, session_key)
+		print "{} created for {}".format(id, runid),
+		dest_path = j(genomics_path, path)
+		if not os.path.exists(dest_path) and os.path.exists(genomics_path):
+			os.makedirs(j(root, path))
+		else:
+			raise Exception("Genomics path can't be found.")
+		for file in files:
+			shutil.copyfiles(file, j(dest_path, os.path.basename(file)))
+			print ", {} copied to {}".format(file, dest_path),
+		print "."
+	except:
+		print "Server rolling back on exception:"
+		server.deferred_accept_rollback(session_key)
+		raise
+	finally:
+		try:
+			server.close_deferred_accept(session_key)
+		except:
+			server.deferred_accept_rollback(session_key)
+			raise
+			
